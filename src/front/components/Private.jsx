8c929cafbc13e794d9a7ed45a5af99357eb2d113
cgerc <gercanepacarmen@gmail.com>
2025-09-02 19:39:14 +0000

    validar token con frontend y backend

--- src/front/components/Private.jsx
+++ src/front/components/Private.jsx
@@ -1,4 +1,49 @@
-import {useEffect} from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
+function Private() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = sessionStorage.getItem('token');
+        if (!token) {
+            navigate('/login');
+            return;
+        }
+    //validar token en el frontend
+    try {
+        const decodedToken= jwtDecode(token);
+        const currentTime= Date.now() / 1000; //tiemp actual en segundos
+        if (decodedToken.exp <currentTime) {
+            sessionStorage.removeItem('token');
+            navigate('/login');
+            return;
+        }
+    } catch (error) {
+        sessionStorage.removeItem('token');
+        navigate('/login');
+        return;
+    }
+
+    //validar token con backend
+    axios
+     .get('http://localhost:5000/private', {
+        headers: {Authorization: `Bearer ${token}`},
+     })
+     .catch(() => {
+        sessionStorage.removeItem('token');
+        navigate('/login');
+     });
+     }, [navigate]);
+
+     return (
+        <div>
+            <h2>Página privada</h2>
+            <p>¡Bienvenido al área privada! Hora acual {new Date().toLocaleString}</p>
+        </div>
+     );
+    }
+
+    export default Private; 
