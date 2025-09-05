4be7bc8f75302f74789e22a2c054fc071dd33000
cgerc <gercanepacarmen@gmail.com>
2025-09-05 03:07:02 +0000

    indentacino

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify, Blueprint
 from flask_jwt_extended import get_jwt_identity, create_access_token, jwt_required, JWTManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from api.models import db, User
 
 app = Flask(__name__)
 api = Blueprint('api', __name__)
@@ -12,30 +13,29 @@ api = Blueprint('api', __name__)
 CORS(app)
 
 # Configuración de la base de datos SQLite
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY']=os.envirot.getenv('SECRET_KEY')
-app.config['JWT_SECRET_KEY'] = os.environ.get('FLASK_APP_KEY', os.urandom(32).hex())
-db = SQLAlchemy(app)
-jwt = JWTManager(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['JWT_SECRET_KEY'] = os.environ.get('FLASK_APP_KEY', os.urandom(32).hex())
+#db = SQLAlchemy(app)
+#jwt = JWTManager(app)
 
 # Modelo de usuario
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+#class User(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    email = db.Column(db.String(120), unique=True, nullable=False)
+#    password = db.Column(db.String(255), nullable=False)
 
 # Crear las tablas de la base de datos
-with app.app_context():
-    db.create_all()
+#with app.app_context():
+#    db.create_all()
 
 # Rutas de la API
 @api.route("/token", methods=["POST"])
 def create_token():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    if email != "test" or password != "test":
-        return jsonify({"msg": "Bad email or password"}), 401
+    ##if email != "test" or password != "test":
+     ##   return jsonify({"msg": "Bad email or password"}), 401
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
 
@@ -44,6 +44,7 @@ def signup():
     data = request.get_json()
     email = data.get('email')
     password = data.get('password')
+    is_active = data.get('is_active')
 
     if not email or not password:
         return jsonify({'msg': "Correo y contraseña son requeridos"}), 400
@@ -53,7 +54,9 @@ def signup():
 
     # Hashear la contraseña
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-    new_user = User(email=email, password=hashed_password)
+    new_user = User(email=email, password=hashed_password, is_active=is_active)
+
+
     try:
         db.session.add(new_user)
         db.session.commit()
@@ -64,15 +67,22 @@ def signup():
 
 @api.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
-    email = data.get('email')
-    password = data.get('password')
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
 
-    user = User.query.filter_by(email=email).first()
-    if not user or not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-        return jsonify({'msg': 'Credenciales inválidas'}), 401
-    access_token = create_access_token(identity=user.email)
-    return jsonify({'access_token': access_token}), 200
+        user = User.query.filter_by(email=email).first()
+
+        # Verificamos primero que exista usuario y que tenga contraseña
+        if not user or not user.password:
+            return jsonify({'msg': 'Credenciales inválidas'}), 401
+
+        # Comparar la contraseña hasheada
+        if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+            return jsonify({'msg': 'Credenciales inválidas'}), 401
+
+        access_token = create_access_token(identity=user.email)
+        return jsonify({'access_token': access_token}), 200
 
 @api.route('/private', methods=['GET'])
 @jwt_required()
